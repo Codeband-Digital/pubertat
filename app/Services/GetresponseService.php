62566b39e79263bf83aa7ad50b0aa88223062b14
Igor Veresov <vi@nimax.ru>
2022-12-13 16:55:43 +0300

    Update getresponse service

--- app/Services/GetresponseService.php
+++ app/Services/GetresponseService.php
@@ -10,6 +10,7 @@ namespace App\Services;
 class GetresponseService extends BaseApiService
 {
     const CASE_1_LEAD_CAMPAIGN_ID = 'oPIZt';
+    const CASE_1_SOLD_CAMPAIGN_ID = 'oPIWi';
 
     public function __construct()
     {
@@ -30,6 +31,41 @@ class GetresponseService extends BaseApiService
     {
         $this->request(
             '/contacts',
+            'POST',
+            ['email' => $email, 'campaign' => ['campaignId' => $campaignId]]
+        );
+    }
+
+    /**
+     * Получение контакта по e-mail и компании.
+     *
+     * @param string $email Адрес электронной почты.
+     * @param string $campaignId Id компании (списка).
+     * @return array
+     * @throws \App\Exceptions\ApiException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getContactByEmailFromCampaign(string $email, string $campaignId): array
+    {
+        return $this->request(
+            "/campaigns/$campaignId/contacts?query[email]=$email",
+            'GET'
+        );
+    }
+
+    /**
+     * Изменение компании у контакта.
+     *
+     * @param string $contactId Id контакта в getresponse.
+     * @param string $email Адрес электронной почты.
+     * @param string $campaignId Id компании (списка).
+     * @throws \App\Exceptions\ApiException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function updateContactCampaign(string $contactId, string $email, string $campaignId): void
+    {
+        $this->request(
+            "POST /contacts/$contactId",
             'POST',
             ['email' => $email, 'campaign' => ['campaignId' => $campaignId]]
         );
